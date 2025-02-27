fc4fe0e3666338989f24e9c7f082331a0e9a278d
itsjayshorey <itsjayshorey@gmail.com>
2025-02-27 18:47:39 +0530

    Updated CSS styles and HTML layout

---
+++ razor_files_demo/Models/Contacts.cs
@@ -0,0 +1,10 @@
+namespace razor_files_demo.Models
+{
+    public class Contacts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
